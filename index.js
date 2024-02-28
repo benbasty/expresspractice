059793abdcfb313d3a4105166c5288852488f700
benbasty <benbasty@yahoo.fr>
2024-02-28 21:45:18 +0800

    nodemon

--- index.js
+++ index.js
@@ -21,6 +21,15 @@ app.get('/dogs', (req,res) => {
     res.send('<h1>For your dogs</h1>');
 })
 
+//adding queries
+//localhost:8080:/search?q=dogs&colors=red
+
+app.get('/search', (req,res) => {
+    console.log(req.query); //q: 'dogs', color: 'red'
+    const {q} = req.query;
+    res.send(`<h1>Search results for: ${q}</h1>`);
+})
+
 app.get('*', (req,res) => {
     console.log('any other routes');
     res.send('<h1>Try again</h1>');
